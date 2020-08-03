73627195bdf30248505913ce81aa5b987d123226
DocMAX <docmax@docmax.my.to>
2020-08-03 19:30:35 +0200

    removed force feedback. changed client/server behaviour.

--- client.py
+++ client.py
@@ -1,36 +1,18 @@
-#!/usr/bin/python
-
-import asyncio
-import pickle
-import base64
-import evdev
-import sys
-import socket
 import argparse
+import base64
+import pickle
+import asyncio
+import evdev
+import socket
 
 
-parser = argparse.ArgumentParser(description="Remote evdev tool 0.1")
+parser = argparse.ArgumentParser(description="evdev-client 0.1")
 parser.add_argument("-s", "--server", help="Server address", required=True)
 parser.add_argument("-d", "--device-name", help="Device to pass to the server", action="append", required=True)
 args = parser.parse_args()
-dev_names = args.device_name
+devs = args.device_name
 srv = args.server
-# dev_names = ["Logitech Logitech RumblePad 2 USB", "Microsoft Microsoft SideWinder Precision Pro (USB)"]
-
-
-def get_device(dev_name):
-    devices_by_name = {}
-    for path in evdev.list_devices():
-        device = evdev.InputDevice(path)
-        devices_by_name[device.name] = device
-    devices = []
-    for name in dev_name:
-        try:
-            devices.append(devices_by_name[name])
-        except KeyError:
-            print("Device \"" + name + "\" does not exist! Aborting...")
-            exit()
-    return devices
+devices = []
 
 
 def unpickle_data(data):
@@ -45,65 +27,33 @@ def pickle_data(data):
     return data
 
 
-async def get_own_events(device, n, queue):
-    async for event in device.async_read_loop():
-        event_list = ["client_event", n, event]
-        queue.put_nowait(event_list)
+async def tcp_client():
+    reader, writer = await asyncio.open_connection(srv, 8888)
 
+    writer.write(pickle_data(["client_devices", devs]))
 
-async def read_server_loop(reader, queue):
     while True:
-        data = await reader.readline()
-        if not data:
-            print("Disconnect")
-            sys.exit()
-        queue.put_nowait(unpickle_data(data))
+        data = unpickle_data(await reader.readline())
+        if data[0] == "srv_device":
+            address = writer.get_extra_info('peername')
+            address_dns = socket.gethostbyaddr(address[0])
+            device = data[1]
+            cap = device.capabilities()
+            del cap[0]
+            devices.append(evdev.UInput(cap, name=device.name + f' (via {address_dns[0]})', vendor=device.info.vendor,
+                                        product=device.info.product))
+            print(f"Created UInput device {device.name} (via {srv})")
+        if data[0] == "srv_dev_event":
+            print(data)
 
 
-async def write_handler(writer, queue):
-    while True:
-        data = await queue.get()
-        if data[0] == "server_event":
-            rumble = evdev.ff.Rumble(strong_magnitude=0x0000, weak_magnitude=0xffff)
-            effect_type = evdev.ff.EffectType(ff_rumble_effect=rumble)
-            duration_ms = 1000
+def main():
 
-            effect = evdev.ff.Effect(
-                evdev.ecodes.FF_RUMBLE, -1, 0,
-                evdev.ff.Trigger(0, 0),
-                evdev.ff.Replay(duration_ms, 0),
-                evdev.ff.EffectType(ff_rumble_effect=rumble)
-            )
-
-            repeat_count = 1
-            effect_id = devices[data[1]].upload_effect(effect)
-            devices[data[1]].write(evdev.ecodes.EV_FF, effect_id, repeat_count)
-            await asyncio.sleep(1)
-            devices[data[1]].erase_effect(effect_id)
-        writer.write(pickle_data(data))
-        await writer.drain()
-
-
-async def client_handler():
-    queue = asyncio.Queue()
     try:
-        reader, writer = await asyncio.open_connection(srv, 8888)
-        address = writer.get_extra_info('peername')
-        address_dns = socket.gethostbyaddr(address[0])
-        print(f"Connected to {address_dns[0]}")
-    except ConnectionRefusedError:
-        print("Connection refused")
-        sys.exit()
-    writer.write(pickle_data(device_list))
-    task_read = asyncio.create_task(read_server_loop(reader, queue))
-    task_write = asyncio.create_task(write_handler(writer, queue))
-    await asyncio.gather(task_read, task_write, *[get_own_events(device, n, queue) for n, device in enumerate(devices)])
+        asyncio.run(tcp_client())
+    except KeyboardInterrupt:
+        pass
 
-devices = get_device(dev_names)
-device_list = ["client_devices", devices]
 
-try:
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(client_handler())
-except (KeyboardInterrupt, ConnectionRefusedError):
-    pass
+if __name__ == "__main__":
+    main()
--- server.py
+++ server.py
@@ -1,10 +1,7 @@
-#!/usr/bin/python
-
-import asyncio
-import pickle
 import base64
+import pickle
+import asyncio
 import evdev
-import socket
 
 
 def unpickle_data(data):
@@ -19,103 +16,59 @@ def pickle_data(data):
     return data
 
 
-async def get_own_events(device, n, queue):
-    # print(f"get_own_events from device {device.name}")
+async def get_dev_events(device, n, queue):
     async for event in device.async_read_loop():
-        event_list = ["server_event", n, event]
+        event_list = ["srv_dev_event", n, event]
         queue.put_nowait(event_list)
-    # print("get_own_events stop")
 
 
-async def read_client_loop(reader, writer, queue, task_write_loop):
-    # print("read_client_loop start")
+async def read_loop(reader, writer, queue, task_write_loop):
+    get_event_tasks = []
     while True:
-        data = await reader.readline()
-        if not data:
-            writer.close()
-            for device in ui_devices:
-                print(f"Removing {device.name}")
-                try:
-                    device.close()
-                except OSError:
-                    pass
-            ui_devices.clear()
-            for task in get_event_tasks:
-                task.cancel()
-            task_write_loop.cancel()
+        try:
+            data = unpickle_data(await reader.readline())
+        except EOFError:
+            for path in paths:
+                print("Unexporting " + path)
             break
-        if data:
-            queue.put_nowait(unpickle_data(data))
-    # print("read_client_loop stop")
+        if data[0] == "client_devices":
+            paths = data[1]
+            for path in data[1]:
+                print("Exporting " + path)
+            for n, path in enumerate(data[1]):
+                device = evdev.InputDevice(path)
+                queue.put_nowait(pickle_data(["srv_device", device]))
+                get_event_tasks.append(asyncio.create_task(get_dev_events(device, n, queue), name="Device" + str(n)))
 
 
 async def write_loop(reader, writer, queue):
-    # print("write_loop start")
     while True:
         data = await queue.get()
-        if not data:
-            break
-        if data[0] == "server_event":
-            event = data[2]
-            if event.code == evdev.ecodes.UI_FF_UPLOAD:
-                try:
-                    upload = ui_devices[data[1]].begin_upload(event.value)
-                    upload.retval = 0
-                    # print(f'[upload] effect_id: {upload.effect_id}, type: {upload.effect.type}')
-                    ui_devices[data[1]].end_upload(upload)
-                    writer.write(pickle_data(data))
-                    await writer.drain()
-                except TypeError:
-                    pass
-            elif event.code == evdev.ecodes.UI_FF_ERASE:
-                try:
-                    erase = ui_devices[data[1]].begin_erase(event.value)
-                    # print(f'[erase] effect_id {erase.effect_id}')
-                    erase.retval = 0
-                    ui_devices[data[1]].end_erase(erase)
-                except TypeError:
-                    pass
-        if data[0] == "client_event":
-            if not ui_devices:
-                pass
-            else:
-                ui_devices[data[1]].write_event(data[2])
-                ui_devices[data[1]].syn()
-        if data[0] == "client_devices":
-            address = writer.get_extra_info('peername')
-            address_dns = socket.gethostbyaddr(address[0])
-            for n, device in enumerate(data[1]):
-                cap = device.capabilities()
-                del cap[0]
-                ui_devices.append(
-                    evdev.UInput(cap, name=device.name + f' (via {address_dns[0]})', vendor=device.info.vendor,
-                                 product=device.info.product))
-                print(f"Created UInput device {device.name} (via {address_dns[0]})")
-            for n, device in enumerate(ui_devices):
-                get_event_tasks.append(asyncio.create_task(get_own_events(device, n, queue), name="Device" + str(n)))
-    # print("write_loop stop")
+        writer.write(data)
 
 
 async def server_handler(reader, writer):
     queue = asyncio.Queue()
     task_write_loop = asyncio.create_task(write_loop(reader, writer, queue))
-    task_read_client_loop = asyncio.create_task(read_client_loop(reader, writer, queue, task_write_loop))
-    await asyncio.gather(task_write_loop, task_read_client_loop)
+    task_read_loop = asyncio.create_task(read_loop(reader, writer, queue, task_write_loop))
+    await asyncio.gather(task_write_loop, task_read_loop)
 
 
-ui_devices = []
-get_event_tasks = []
-loop = asyncio.get_event_loop()
-coro = asyncio.start_server(server_handler, '0.0.0.0', 8888)
-server = loop.run_until_complete(coro)
-addr = server.sockets[0].getsockname()
-print(f'Serving on {addr}')
+def main():
+    loop = asyncio.get_event_loop()
+    coro = asyncio.start_server(server_handler, '0.0.0.0', 8888)
+    server = loop.run_until_complete(coro)
+    addr = server.sockets[0].getsockname()
+    print(f'Serving on {addr}')
 
-try:
-    loop.run_forever()
-except KeyboardInterrupt:
-    pass
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        print("Caught keyboard interrupt. Canceling tasks...")
+    finally:
+        for task in asyncio.all_tasks():
+            task.cancel()
 
-server.close()
-loop.run_until_complete(server.wait_closed())
-loop.close()
+
+if __name__ == "__main__":
+    main()
